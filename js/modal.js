3584cc4cb3169a5da4e7146f6f371ef365cbf6b6
M1CH4EL-ux2 <joaomichael912@gmail.com>
2022-04-01 08:52:06 -0300

    Ajuste de modal

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,11 @@
-const button = document.querySelector('.btnShow');
-const modal = document.querySelector('.bgModal')
+const open = document.querySelector('.btnShow');
+const close = document.querySelector('.closeModal');
+
+const modal = document.querySelector('.bgModal');
 
 const visibility = () => {
     modal.classList.toggle('show')
 }
 
-button.addEventListener('click', visibility)
+open.addEventListener('click', visibility);
+close.addEventListener('click', visibility);
